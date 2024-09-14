25f4c2b09311a5ae73826b6128a52bfc24a13ac5
Divyansh Ajay <divyanshajay@gmail.com>
2024-09-14 23:47:42 +0530

    changes made

---
+++ basics/index.js
@@ -0,0 +1,35 @@
+console.log('this code is just for practice, I am going through JS basics');
+var a=0;
+let name='divyansh';
+const name1 ='divyanshajay';
+
+name ='honey';
+
+
+console.log(name);
+console.log(name1);
+let users=['divyansh','ansh','daksh','aaryan'];
+for(let i=0;i<users.length;i++){
+	console.log(users[i]);
+}
+
+
+let age = 19;
+let canVote = (age>18);
+console.log(canVote);
+
+
+function greet(name){
+	return "hello " + name;
+	
+}
+
+
+console.log(greet('honey'));
+
+
+// objects in js
+
+
+
+
